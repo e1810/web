77e727ff27637dfb1420bc1ecc5b7923b8743474
e1810 <v.iceELE1810@gmail.com>
2019-07-04 12:05:40 +0900

    header became rotatable

--- intro.js
+++ intro.js
@@ -1,7 +1,14 @@
 (function(){
-    "use strict";
-    var header = document.getElementById("header");
-    header.style.transform = "rotateX(60deg)";
-
-
+	"use strict";
+	var header = document.getElementById("header");
+	var deg = 0;
+	
+	function rotateHeader(){
+		deg += 6;
+		deg %= 360;
+		if(90 < deg && deg < 270) header.className = "back";
+		else header.className = "face";
+		header.style.transform = "rotateX(" + deg + "deg)";
+	}
+	setInterval(rotateHeader, 20);
 })();
