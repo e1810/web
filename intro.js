4ca05d1b3a087dd0b2451788c09e5098e11abb9e
e1810 <e1810@s.akashi.ac.jp>
2019-07-04 21:05:12 +0900

    add animation and generater(only surface)

--- intro.js
+++ intro.js
@@ -1,14 +1,38 @@
 (function(){
 	"use strict";
 	var header = document.getElementById("header");
-	var deg = 0;
-	
+	var xdeg = 0;
+	var ydeg = 0;
+	var xy = 0;
+	var faceback = 0;
+
+
 	function rotateHeader(){
-		deg += 6;
-		deg %= 360;
-		if(90 < deg && deg < 270) header.className = "back";
-		else header.className = "face";
-		header.style.transform = "rotateX(" + deg + "deg)";
+		faceback = 0;
+		if (xy) {
+			ydeg += 6;
+			ydeg %= 360;
+			if(90 <= xdeg && xdeg <= 270) faceback += 1;
+			if(90 < ydeg && ydeg < 270) faceback += 1;
+			
+			header.style.transform = "rotateX(" + xdeg + "deg) rotateY(" + ydeg + "deg)";
+		
+			if (ydeg == 180 || ydeg == 0) xy = 0;
+		}
+		else{
+			xdeg += 6;
+			xdeg %= 360;
+			if(90 <= xdeg && xdeg <= 270) faceback += 1;
+			if(90 < ydeg && ydeg < 270) faceback += 1;
+		
+			header.style.transform = "rotateX(" + xdeg + "deg) rotateY(" + ydeg + "deg)";
+		
+			if (xdeg == 180 || xdeg == 0) xy = 1;
+			
+		}
+		if (faceback==0) header.className = "face";
+		else if (faceback==1) header.className = "back";
+		else header.className = "true";
 	}
-	setInterval(rotateHeader, 20);
+	setInterval(rotateHeader, 30);
 })();
